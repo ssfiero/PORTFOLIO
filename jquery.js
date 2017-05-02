75cf7ef5bc1e7bee4c4274efc8b634ab29eebfe1
ssfiero <ssfiero@yahoo.com>
2017-05-01 23:18:14 -0500

    Added more projects

---
+++ jquery.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){
+//   $(".top-projects").hover(function(){
+//     $(".top-projects h3").css({"position": "absolute", "margin-bottom": "-100px"});
+//       });
+// });
