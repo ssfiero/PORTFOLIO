4880ab20975dd767d1cbd35c993a9f2bb14279b8
Steven Fiero <stevensfiero@gmail.com>
2017-10-12 09:33:23 -0500

    Added JQuery and carousel

--- jquery.js
+++ jquery.js
@@ -1,5 +1,58 @@
-// $(document).ready(function(){
-//   $(".top-projects").hover(function(){
-//     $(".top-projects h3").css({"position": "absolute", "margin-bottom": "-100px"});
-//       });
-// });
+$(document).ready(function() {
+
+  // $('.navbar').hide();
+
+  // Toggle CSS classes on scroll to hide/show background-color on navbar.
+  $(window).scroll(function() {
+    $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 1);
+    // $('.navbar').show($(this).scrollTop > 1);
+  });
+
+
+  // Toggle CSS classes on scroll to hide/show section with icons.
+  // $(window).scroll(function() {
+  //   $('.additional h2').toggleClass('scrolled', $(this).scrollTop() > 150);
+  //   $('#ico .icon').toggleClass('scrolled', $(this).scrollTop() > 150);
+  // });
+
+
+  // Hide/Show on scroll section with icons.
+  $(window).scroll(function() {
+    var windowScrollTop = $(this).scrollTop();
+
+    if (windowScrollTop < 100) {
+      $('.additional h2').css({'display': 'none'});
+      $('#ico .icon').css({'display': 'none'});
+        } else {
+          $('.additional h2').css({'display': 'block'});
+          $('#ico .icon').css({'display': 'block'});
+    }
+  });
+
+
+  // Hide projects button after DOM load.
+  $('.top-projects .btn-info').hide();
+
+  function showButton() {
+    $(this).closest('.top-projects').find('.btn-info').css({'display': 'block'});
+    $(this).closest('.top-projects').find('.btn-info').css({'margin-top': '-275px'});
+  }
+
+  function hideButton() {
+    $(this).closest('.top-projects').find('.btn-info').css({'display': 'none'});
+  }
+
+  $('.top-projects').on('mouseenter', showButton);
+  $('.top-projects').on('mouseleave', hideButton);
+
+
+  // JQuery adds qoutes from quotes.html using the random generator.
+  // $(function() {
+  //   $('generateRandomWord').load('quotes.html', function() {
+  //     jQuery('.item').addClass('active');
+  //   });
+  // });
+
+
+
+});
